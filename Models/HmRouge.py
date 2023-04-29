17861a6a969366487dbde9a49225d55db0ff7571
PraveenBushipaka <praveenbushipaka942@gmail.com>
2023-04-30 00:36:50 +0200

    modularized code

---
+++ Models/HmRouge.py
@@ -0,0 +1,74 @@
+from nltk.stem import SnowballStemmer
+from rouge import Rouge
+from ItaRouge import *
+import pandas as pd
+import numpy as np
+
+
+def calc_hm_rouge(df):
+    ''' 
+    Calculate rouge scores for Arithmetic mean
+    Args:
+      df: dataframe
+    Returns:
+      final_scores: array of rouge scores  '''
+    evaluator = ItaRouge(
+        metrics=["rouge-n"],
+        max_n=2,
+        apply_avg=False,
+        apply_best=False,
+        weight_factor=1.2,
+        stemming=True,
+    )
+    all_hypothesis = df.sentence.values
+    all_references = df.summary.values
+
+    scores = evaluator.get_scores(all_hypothesis, all_references)
+    scores_avg = {}
+    for metric, results in sorted(scores.items(), key=lambda x: x[0]):
+        scores_avg[metric] = []
+        for hypothesis_id, results_per_ref in enumerate(results):
+            nb_references = len(results_per_ref["p"])
+            for reference_id in range(nb_references):
+                scores_avg[metric].append(results_per_ref["f"][reference_id] * 100.0)                
+    #Here we calculate Harmonic mean of R-1 and R-2 scores
+    m = np.array(scores_avg["rouge-1"]) * np.array(scores_avg["rouge-2"])
+    s = np.array(scores_avg["rouge-1"]) + np.array(scores_avg["rouge-2"])
+    sc = np.nan_to_num(2 * m / s)
+
+    final_scores = sc
+    return final_scores
+
+
+def pre_processing(df):
+    '''
+    Preprocessing the data
+    Args: 
+      df: dataframe
+    Returns:
+      df_sentences: dataframe with each sentence as a row
+    '''
+    df = df.drop_duplicates(subset=["summary"], keep="first")
+    df["sentence"] = df.doc.apply(lambda x: x.split("\n"))
+    df_sentences = df.explode("sentence")
+    df_sentences["sentence"].replace("", np.nan, inplace=True)
+    df_sentences.dropna(inplace=True)
+    return df_sentences
+
+
+def generate_hm_rouge_scores(df):
+    ''' 
+    generate rouge scores for each sentence in the dataframe 
+    Args:
+      df: dataframe
+    Returns:
+      df_sentences: dataframe with each sentence as a row and rouge scores as a column
+    '''
+    df_sentences = pre_processing(df)
+    scores = calc_hm_rouge(df_sentences)
+    df_sentences["scores"] = scores
+    df_sentences.rename(columns={"sentence": "text", "scores": "label"}, inplace=True)
+    df_sentences = df_sentences[["text", "label"]]
+    df_sentences["text"].replace("", np.nan, inplace=True)
+    df_sentences.dropna(inplace=True)
+    return df_sentences
---
+++ Models/ItaRouge.py
@@ -0,0 +1,33 @@
+from rouge import Rouge
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+nltk.download("punkt")
+import pandas as pd
+import numpy as np
+
+
+class ItaRouge(Rouge):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lang = "italian"
+        Rouge.STEMMER = stemmer_snowball = SnowballStemmer(self.lang)
+
+    def _preprocess_summary_as_a_whole(self, summary):
+        """
+        Preprocessing (truncate text if enable, tokenization, stemming if enable, lowering) of a summary as a whole
+        Args:
+          summary: string of the summary
+        Returns:
+          Return the preprocessed summary (string)
+        """
+
+        summary = Rouge.REMOVE_CHAR_PATTERN.sub(" ", summary.lower()).strip()
+
+        tokens = super().tokenize_text(summary, self.lang)
+
+        if self.stemming:
+            self.stem_tokens(tokens)  # stemming in-place
+
+        preprocessed_summary = [" ".join(tokens)]
+
+        return preprocessed_summary
